245fd6a69eb8c4cbe1e10d639dff423a6da13326
Xing Meng Yu <Simon@Xings-MBP.lan>
2021-04-14 00:23:23 -0500

    further pushing timeout

--- tests/amazon/amazon-search-v1.js
+++ tests/amazon/amazon-search-v1.js
@@ -1,7 +1,7 @@
 module.exports = {
     'Navigate to Amazon homepage': function (browser) {
         browser.url('https://www.amazon.com/');
-        browser.waitForElementVisible('[id="gw-desktop-herotator"]', 20000);
+        browser.waitForElementVisible('[id="gw-desktop-herotator"]', 60000);
     },
 
     'Search for product 功夫茶': function(browser) {
