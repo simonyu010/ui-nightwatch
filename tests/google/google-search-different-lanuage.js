7d9df6b78b87d4df25e87ad25d584095dab5112e
hongpeng yang <yanghongpeng54@gmail.com>
2021-04-16 00:36:13 -0500

    clearValue practice

--- tests/google/google-search-different-lanuage.js
+++ tests/google/google-search-different-lanuage.js
@@ -5,7 +5,11 @@ module.exports = {
     },
 
     'Search for invalid symbol': function(browser) {
-        browser.setValue('[class="gLFyf gsfi" ]', ['我的宝宝是个猪',browser.Keys.ENTER]);
+        browser.setValue('[class="gLFyf gsfi" ]', ['我的宝宝是个猪']);
         browser.pause(2000);
+        browser.clearValue('[class="gLFyf gsfi" ]');
+        browser.pause(2000);
+        browser.setValue('[class="gLFyf gsfi" ]', ['我的宝宝还是个猪']);
+        browser.pause(5000);
     }
 }
