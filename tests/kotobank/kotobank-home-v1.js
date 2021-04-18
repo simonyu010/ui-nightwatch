b34551dc9e571c9f222f5b87d6ae846db099c4ef
Henry Chou <henrychou@HenrydeMacBook-Pro.local>
2021-04-18 09:35:07 +0800

    add voyage tab check

--- tests/kotobank/kotobank-home-v1.js
+++ tests/kotobank/kotobank-home-v1.js
@@ -10,5 +10,14 @@ module.exports = {
         browser.setValue('[id="search_input"]', 'コトバンク');
         browser.click('[type="submit"]');
         browser.pause(2000);
+    },
+
+    'Check VOYAGE tab': function (browser) {
+        browser.url('https://kotobank.jp/');
+        browser.pause(2000)
+        browser.click('[id="ftWrap"] > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > img:nth-child(1)');
+        browser.waitForElementVisible('[src="assets/img/vmlogo_white.png"]', 15000);
+        browser.expect.element('[id="pc-menu"] > ul:nth-child(2) > li:nth-child(2) > a').text.to.contain('会社概要');
+        browser.pause(2000);
     }
 }
