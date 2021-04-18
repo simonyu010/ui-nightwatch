a27f52c3a383e9753d3704c2c77f85bbe9300993
Henry Chou <henrychou@HenrydeMacBook-Pro.local>
2021-04-18 08:49:49 +0800

    add assertion of url and toplogo

--- tests/kotobank/kotobank-home-v1.js
+++ tests/kotobank/kotobank-home-v1.js
@@ -1,12 +1,14 @@
 module.exports = {
     'Navigate to Kotobank homepage': function (browser) {
         browser.url('https://kotobank.jp/');
+        browser.assert.urlContains('kotobank.jp')
+        browser.verify.containsText('[id="toplogo"]', 'コトバンク');
         browser.waitForElementVisible('[id="toplogo"]', 15000);
     },
 
     'Search for コトバンク': function(browser) {
         browser.setValue('[id="search_input"]', 'コトバンク');
         browser.click('[type="submit"]');
-        browser.pause(5000);
+        browser.pause(2000);
     }
 }
