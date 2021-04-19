d9323a381cf1d78211b311cd005926bb8b2772e2
Xing Meng Yu <Simon@Xings-MBP.lan>
2021-04-18 20:51:43 -0500

    added comments

--- tests/kotobank/kotobank-home-v1.js
+++ tests/kotobank/kotobank-home-v1.js
@@ -1,9 +1,9 @@
 module.exports = {
     'Navigate to Kotobank homepage': function (browser) {
         browser.url('https://kotobank.jp/');
-        browser.assert.urlContains('kotobank.jp')
-        browser.verify.containsText('[id="toplogo"]', 'コトバンク');
-        browser.waitForElementVisible('[id="toplogo"]', 15000);
+        browser.assert.urlContains('kotobank.jp') //use verify
+        browser.verify.containsText('[id="toplogo"]', 'コトバンク'); //wait for should happen before this line
+        browser.waitForElementVisible('[id="toplogo"]', 15000); 
     },
 
     'Search for ポケモン': function(browser) {
@@ -12,7 +12,7 @@ module.exports = {
         browser.setValue('[id="search_input"]', 'ポケモン');
         browser.click('[type="submit"]');
         browser.waitForElementVisible('[id="topicPath"] > li:nth-child(2)', 15000);
-        browser.assert.containsText('[id="topicPath"] > li:nth-child(2)', 'ポケモン');
+        browser.assert.containsText('[id="topicPath"] > li:nth-child(2)', 'ポケモン'); //use verify
     },
 
     'Check VOYAGE tab': function (browser) {
@@ -21,7 +21,7 @@ module.exports = {
         browser.refresh();
         browser.waitForElementVisible('[id="toplogo"]', 15000);
         //use a better selector
-        browser.click('[id="sponsor"] img');
+        browser.click('[id="sponsor"] img'); //wait for this element before clicking instead of top logo
         browser.waitForElementVisible('[src="assets/img/vmlogo_white.png"]', 15000);
         browser.expect.element('[id="pc-menu"] > ul:nth-child(2) > li:nth-child(2) > a').text.to.contain('会社概要');
     }
