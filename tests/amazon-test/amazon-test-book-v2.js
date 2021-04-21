6290e615ed4e5cdaa960582cd49cace64fb2cb41
hongpeng yang <yanghongpeng54@gmail.com>
2021-04-20 22:51:19 -0500

    clean up step 2 and 3

--- tests/amazon-test/amazon-test-book-v2.js
+++ tests/amazon-test/amazon-test-book-v2.js
@@ -1,6 +1,7 @@
 module.exports = {
     'Navigate to Amazon homepage': function (browser) {
         const amazonHome = browser.page.amazonHomePage();
+
         amazonHome.navigate();
         amazonHome.waitForElementVisible('@mainAppFrame', 15000);
     },
@@ -8,19 +9,23 @@ module.exports = {
     'Search for book by ISBN-10': function (browser) {
         const amazonHeader = browser.page.amazonCommonPage();
         const afterSearch = browser.page.amazonAfterSearchPage();
+
+        amazonHeader.verify.visible('@searchBar');
         amazonHeader.setValue('@searchBar', '0745656064');
         browser.keys(browser.Keys.ENTER);
         afterSearch.verify.containsText('@bookName', 'Digital Media Ethics');
     },
 
-    // 'Search for book by ISBN-13': function (browser) {
-    //     const amazon = browser.page.amazon();
-    //     amazon.click('@searchBar');
-    //     amazon.clearValue('@searchBar');
-    //     amazon.setValue('@searchBar', '978-0745656069');
-    //     browser.keys(browser.Keys.ENTER);
-    //     amazon.verify.containsText('@bookName', 'Digital Media Ethics');
-    // },
+    'Search for book by ISBN-13': function (browser) {
+        const amazonHeader = browser.page.amazonCommonPage();
+        const amazonResultPage = browser.page.amazonAfterSearchPage();
+
+        amazonHeader.waitForElementVisible('@searchBar');
+        amazonHeader.clearValue('@searchBar');
+        amazonHeader.setValue('@searchBar', '978-0745656069');
+        browser.keys(browser.Keys.ENTER);
+        amazonResultPage.verify.containsText('@bookName', 'Digital Media Ethics');
+    },
 
     // 'Add to cart function': function (browser) {
     //     const amazon = browser.page.amazon();
