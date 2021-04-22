c8158d54937ef5e6fb0c094cc4f7d18fecc9f8a4
Henry Chou <henrychou@HenrydeMacBook-Pro.local>
2021-04-22 08:28:47 +0800

    add map search v2

--- page-objects/googleHomePage.js
+++ page-objects/googleHomePage.js
@@ -4,7 +4,7 @@ module.exports = {
         searchBar: { selector: 'div:nth-child(1) div:nth-child(2) input' },
         firstSuggestion: { selector: 'li:nth-child(1)>div' },
         searchResults: { selector: '[id="search"]' },
-        mapPicture: { selector: '[id="dimg_16"]' },
-        fullPicture: { selector: '[id="rcnt"]' }
+        navigateButton: { selector: '[style="cursor:pointer"] > div:nth-child(1)' },
+        destinationInput: { selector: '[id="directions-searchbox-1"] input' },
     }
 }
