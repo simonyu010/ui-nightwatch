d2f3a9ca52cddc001451e0f233d9e2aa33e69293
Xing Meng Yu <Simon@Xings-MBP.lan>
2021-04-21 20:38:47 -0500

    refactor validation using getValue instead of getText

--- tests/google/google-search-map-v2.js
+++ tests/google/google-search-map-v2.js
@@ -1,45 +1,39 @@
 module.exports = {
     'Navigate to Google homepage': function (browser) {
-        const google = browser.page.googleHomePage()
+        const google = browser.page.googleHomePage();
 
         google.navigate();
         google.waitForElementVisible('@searchBar', 15000);
     },
 
     'Test Suggestion list fuction': function (browser) {
-        const google = browser.page.googleHomePage()
+        const google = browser.page.googleHomePage();
 
-        google.click('@searchBar');
+        google.verify.visible('@searchBar');
         google.setValue('@searchBar', '1600 Pennsylvania Ave NW Washington, DC 20500');
         google.waitForElementVisible('@firstSuggestion', 8000);
         google.verify.containsText('@firstSuggestion', '1600 pennsylvania');
-        browser.pause(1000);
-    },
-
-    'Test Map Address': function (browser) {
-        const google = browser.page.googleHomePage()
-
         browser.keys(browser.Keys.ENTER);
         google.verify.containsText('@searchResults', '1600 Pennsylvania');
     },
 
     'Test Navigate Button': function (browser) {
-        const google = browser.page.googleHomePage()
+        const google = browser.page.googleHomePage();
 
-        google.waitForElementPresent('@navigateButton', 15000);
+        google.waitForElementVisible('@navigateButton', 15000);
         google.click('@navigateButton');
         google.waitForElementNotPresent('@navigateButton', 15000);
     },
-
+    
     'Test Navigate Page': function (browser) {
         let address;
-        const google = browser.page.googleHomePage()
+        const google = browser.page.googleHomePage();
 
         google.waitForElementPresent('@destinationInput', 15000);
-        google.getText('@destinationInput', function(result){
+        browser.getValue('[id="directions-searchbox-1"] input', function(result){
             address=result.value;
             console.log('++++++++++++++++++++++++++++++++++++++', address);
         });
-        // google.verify.containsText('@destinationInput', '1600');
+        google.verify.valueContains('@destinationInput', '1600');
     }
 }
