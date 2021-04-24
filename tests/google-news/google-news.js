59b98d609423a7576759533f9cd8c49ccb3f07ca
hongpeng yang <yanghongpeng54@gmail.com>
2021-04-24 18:00:05 -0500

    google news test clean up and replace with stable selector

--- tests/google-news/google-news.js
+++ tests/google-news/google-news.js
@@ -1,19 +1,26 @@
 module.exports = {
     'Verify user can navigate to Google homepage by URL': function (browser) {
-        browser.url('https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en');
-        browser.waitForElementVisible('[class="OHST9e"]', 15000);
+        browser.url('https://google.com/');
+        browser.verify.visible('[height="92"]');
     },
 
-    'Navigate to google news science': function (browser) {
-        browser.verify.visible('[jsname="V2bVMb"] div:nth-child(11) div:nth-child(2)');
-        browser.click('[jsname="V2bVMb"] div:nth-child(11) div:nth-child(2)');
-        browser.verify.visible('[class="Ax4B8 ZAGvjd"]');
-        browser.click('[class="Ax4B8 ZAGvjd"]');
-        
-    'Verify google news search result'
-        browser.verify.visible('[class="Ax4B8 ZAGvjd"]');
-        browser.setValue('[class="Ax4B8 ZAGvjd"]', 'nasa shares photos of lunar gateway space station to launch for moon');
-        browser.verify.containsText();
-        browser.pause(4000);
+    'Navigate to google news science from homepage': function (browser) {
+        browser.waitForElementVisible('[maxlength="2048"]');
+        browser.setValue('[maxlength="2048"]', 'google news');
+        browser.keys(browser.Keys.ENTER);
+        browser.waitForElementVisible('[id="rso"] div div div div div div h3', 15000);
+        browser.click('[id="rso"] div div div div div div h3');
+        browser.verify.visible('[jsdata="deferred-i3"] div div');
+
+        'Verify google news Science button'
+        browser.waitForElementVisible('header c-wiz div:nth-child(10)');
+        browser.click('header c-wiz div:nth-child(10)');
+        // browser.verify.visble('[class="OJMBqe"]');
+
+        'Verify google new search result'
+        browser.waitForElementVisible('[jsaction^="input"]',15000);
+        browser.setValue('[jsaction^="input"]', 'nasa shares photos of lunar gateway space station to launch for moon');
+        browser.keys(browser.Keys.ENTER);
+        browser.verify.containsText('[data-n-et="406"] div div div h3', 'shares photos');
     }
 }
