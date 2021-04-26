b4523cc7feae15cb8d583c82a9bdc2e7482c6d61
hongpeng yang <yanghongpeng54@gmail.com>
2021-04-26 18:45:28 -0500

    switch wait for searchbar to forecastElement

--- tests/google-news/google-news.js
+++ tests/google-news/google-news.js
@@ -8,20 +8,21 @@ module.exports = {
 
     'Verify user can navigate to google news from search result': function (browser) {
         const googleHome = browser.page.googleHomePage();
-        const googleNews = browser.page.googleNewsPage();
         const googleResult = browser.page.googleResultPage();
 
         googleHome.verify.visible('@searchBar');
         googleHome.setValue('@searchBar', 'google news');
         browser.keys(browser.Keys.ENTER);
-        googleNews.waitForElementVisible('@forecastElement');
+        googleResult.waitForElementVisible('@searchResult', 15000);
         googleResult.click('@searchResult');
+        
     },
 
     'Verify google news search result': function (browser) {
         const googleNewsResult = browser.page.googleNewsResultPage();
+        const googleNews = browser.page.googleNewsPage();
 
-        googleNewsResult.waitForElementVisible('@searchBar', 15000);
+        googleNews.waitForElementVisible('@forecastElement');
         googleNewsResult.setValue('@searchBar', 'nasa shares photos of lunar gateway space station to launch for moon');
         browser.keys(browser.Keys.ENTER);
         googleNewsResult.verify.containsText('@searchResult', 'shares photos');
